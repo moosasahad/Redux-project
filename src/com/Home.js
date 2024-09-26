d0bf2dbe08da142fbbf121ef08a960de7ce908ba
moosa sahad <sahadmoosa018@gmail.com>
2024-09-26 15:37:54 +0530

    allmost-complited

---
+++ src/com/Home.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useDispatch,useSelector} from 'react-redux'
+import { inputfeeld,addbutton,removebutton,editbutton,saveedits } from '../redux/todoSlice'
+import './Home.css';
+function Home() {
+    const dispatch = useDispatch()
+    const inputbar = (e)=>{
+        dispatch(inputfeeld(e.target.value))
+    }
+    const inputvalue = useSelector((state)=>state.todo.inputvalue)
+    const item = useSelector((state)=>state.todo.addvalue)
+    const editedid = useSelector((state)=>state.todo.editedid)
+  return (
+    <div className='maindiv'>
+     <div className='seconddiv'>
+        <input value={inputvalue} onChange={inputbar} type="text" />
+        {
+            !editedid ? (<button onClick={()=>dispatch(addbutton())}>Add</button>)
+            :
+            (<button onClick={()=>dispatch(saveedits())}>save</button>)
+        }
+     
+            <div className='todolist'>
+                {
+                    item.map(item=>(
+                        
+                            <li className="itemlist">
+                            {item.text}
+                            <div className="buttons">
+                            <button onClick={()=>dispatch(editbutton(item.id))}>Edite</button>
+                            <button onClick={()=>dispatch(removebutton(item.id))}>Remove</button>
+                            </div>
+                        </li>
+                       
+                    ))
+                }
+                
+            </div>
+        </div>
+     </div>
+  )
+}
+
+export default Home
